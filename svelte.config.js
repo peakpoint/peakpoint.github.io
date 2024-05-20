ad5021a38b458e1ba3d03c1485b61e48322eba5e
peakpoint <27919714+peakpoint@users.noreply.github.com>
2024-05-19 22:18:59 -0400

    swap order of preprocessors

--- svelte.config.js
+++ svelte.config.js
@@ -67,8 +67,8 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: [
-    vitePreprocess(),
     mdsvex(mdsvexConfig),
+    vitePreprocess(),
     preprocess({
       scss: {
         prependData: '@use "src/variables.scss" as *;',
